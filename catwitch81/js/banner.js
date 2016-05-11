4193f6c9ae57f5ee67a7b341cbe583143d0f3da0
catwitch81 <catwitch81@gmail.com>
2016-05-11 09:50:54 +0900

    Add files via upload

---
+++ catwitch81/js/banner.js
@@ -0,0 +1,25 @@
+function banner(){
+         var s_time = 1000,
+            s2_time = 2000;
+    $('.left_btn').on('click', function(){
+        var banner_li = $('.banner_box').children('li'),
+            banner_li_width = banner_li.width();
+       banner_li.first().stop().animate({marginLeft: banner_li_width}, s_time, function(){
+           banner_li.css({marginLeft: 0}).last().prependTo('.banner_box');
+       }); 
+    });
+    $('.right_btn').on('click', function(){
+        var banner_li = $('.banner_box').children('li'),
+            banner_li_width = banner_li.width();
+       banner_li.first().stop().animate({marginLeft: -banner_li_width}, s_time, function(){
+           banner_li.css({marginLeft: 0}).first().appendTo('.banner_box');
+       }); 
+    });
+    var timer;
+    // 일정한 시간 간격을 두고 움직이는 기능 : setInterval
+    function startBtn(){ timer = setInterval( '$(".right_btn").click()', s2_time ) };
+    startBtn();
+    // setInterval을 없애는 기능
+    function stopBtn(){ clearInterval( timer )};
+    $('#banner').on({'mouseover':stopBtn, 'mouseout':startBtn });
+};
